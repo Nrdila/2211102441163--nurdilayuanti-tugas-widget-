437e17b18842e0939f193dd7b0f95323e665ca15
Nrdila <164182313+Nrdila@users.noreply.github.com>
2024-03-20 23:38:48 +0800

    Update container

--- flutter_widgets/lib/homepage.dart
+++ flutter_widgets/lib/homepage.dart
@@ -66,4 +66,30 @@ class TextWidget extends StatelessWidget {
     );
   }
 }
+class ContainerWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          '2. Container:',
+          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+        ),
+        Container(
+          width: 100.0,
+          height: 100.0,
+          color: Color.fromARGB(255, 1, 215, 223),
+          child: const Center(
+            child: Text(
+              'Universitas Muhammadiyah Kalimantan Timur',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        SizedBox(height: 20.0),
+      ],
+    );
+  }
+}
 
